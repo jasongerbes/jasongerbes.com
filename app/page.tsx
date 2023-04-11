1aa9bfb9a8110dcbe075b43d78d3dd61366e1589
Jason Gerbes <j.gerbes@me.com>
2023-04-11 18:15:49 +1200

    Tweak spacing

--- app/page.tsx
+++ app/page.tsx
@@ -4,42 +4,44 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div className="mx-auto max-w-7xl px-8 py-20">
-      <div className="max-w-2xl">
-        <Image
-          className="h-16 w-16 rounded-full md:h-24 md:w-24"
-          src="/images/profile-image.jpg"
-          alt="Portrait photo of the website's author, Jason Gerbes"
-          width={96}
-          height={96}
-        />
+    <div className="px-4 py-16 sm:px-8 sm:py-20">
+      <div className="mx-auto max-w-7xl">
+        <div className="max-w-2xl">
+          <Image
+            className="h-16 w-16 rounded-full md:h-24 md:w-24"
+            src="/images/profile-image.jpg"
+            alt="Portrait photo of the website's author, Jason Gerbes"
+            width={96}
+            height={96}
+          />
 
-        <Title className="mt-8">
-          Software architect, technology enthusiast, and cat dad.
-        </Title>
+          <Title className="mt-8">
+            Software architect, technology enthusiast, and cat dad.
+          </Title>
 
-        <p className="mt-6 text-base text-gray-600 dark:text-gray-400">
-          I’m Jason, a front-end software architect based in the beautiful city
-          of Auckland, New Zealand. I have a passion for technology, design, and
-          sustainability. Follow my journey by checking out my recent blog
-          posts. Welcome!
-        </p>
-      </div>
+          <p className="mt-6 text-base text-gray-600 dark:text-gray-300">
+            I’m Jason, a front-end software architect based in the beautiful
+            city of Auckland, New Zealand. I have a passion for technology,
+            design, and sustainability. Follow my journey by checking out my
+            recent blog posts. Welcome!
+          </p>
+        </div>
 
-      <div className="mt-16 max-w-2xl">
-        <h2 className="text-md font-semibold uppercase tracking-tight text-teal-800 dark:text-teal-700">
-          Latest Posts
-        </h2>
+        <div className="mt-16 max-w-2xl">
+          <h2 className="text-md font-semibold uppercase tracking-tight text-teal-800 dark:text-teal-600">
+            Latest Posts
+          </h2>
 
-        <ul className="mt-4">
-          <li>
-            <BlogPost />
-            <BlogPost />
-            <BlogPost />
-            <BlogPost />
-            <BlogPost />
-          </li>
-        </ul>
+          <ul className="mt-4">
+            <li>
+              <BlogPost />
+              <BlogPost />
+              <BlogPost />
+              <BlogPost />
+              <BlogPost />
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   )
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,17 +5,17 @@ export function Footer() {
   const year = new Date().getFullYear()
 
   return (
-    <footer className="bg-teal-100 px-6 pb-20 pt-16 dark:bg-teal-950/30 sm:px-8">
-      <div className="mx-auto flex max-w-7xl flex-col justify-between gap-10 sm:flex-row">
+    <footer className="bg-teal-100 px-8 pb-20 pt-16 dark:bg-teal-950/30 sm:px-8">
+      <div className="mx-auto flex max-w-7xl flex-col justify-between gap-12 sm:flex-row">
         <nav>
-          <ul className="flex flex-col gap-8 text-sm font-medium sm:flex-row">
+          <ul className="flex flex-col gap-6 text-sm font-medium sm:flex-row">
             <NavLink href="/">Home</NavLink>
             <NavLink href="/about">About</NavLink>
             <NavLink href="/blog">Blog</NavLink>
             <NavLink href="/contact">Contact</NavLink>
           </ul>
         </nav>
-        <p className="text-center text-sm">
+        <p className="text-center text-sm text-gray-600 dark:text-gray-300">
           &copy; {year} Jason Gerbes. All rights reserved.
         </p>
       </div>
