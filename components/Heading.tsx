2882f6a81a94ec972503132ea97edfad86ebb92f
Jason Gerbes <j.gerbes@me.com>
2023-04-18 23:10:37 +1200

    Remove rehype-autolink-headings (#8)
    * Move anchor tags to end of headings
    
    * Move anchors back to start of heading to prevent unwanted wrapping

--- components/Heading.tsx
+++ components/Heading.tsx
@@ -1,10 +1,30 @@
+import { Link as LinkIcon } from '@/assets/phosphor-icons'
+import clsx from 'clsx'
+
 export type HeadingLevel = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
 
-export interface HeadingProps extends React.HTMLAttributes<HTMLHeadingElement> {
+export type HeadingProps = React.HTMLAttributes<HTMLHeadingElement> & {
   level: HeadingLevel
-  children: React.ReactNode
 }
 
 export function Heading({ level: Tag, children, ...props }: HeadingProps) {
-  return <Tag {...props}>{children}</Tag>
+  return (
+    <Tag {...props} className={clsx(props.className, props.id && 'group')}>
+      {props.id && <HeadingAnchorLink id={props.id} />}
+      {children}
+    </Tag>
+  )
+}
+
+function HeadingAnchorLink({ id }: { id: string }) {
+  return (
+    <a
+      className="absolute inset-y-0 left-0 hidden translate-x-[-100%] flex-col justify-center px-1 no-underline opacity-0 transition-all group-hover:opacity-100 sm:flex xl:px-2"
+      href={`#${id}`}
+      aria-hidden="true"
+      tabIndex={-1}
+    >
+      <LinkIcon width={24} height={24} aria-hidden="true" />
+    </a>
+  )
 }
