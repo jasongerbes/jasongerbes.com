6100940c4019f9586fa0e034d2a36794a6d688ae
Jason Gerbes <j.gerbes@me.com>
2023-04-19 16:45:10 +1200

    Improve header styling and hide empty prose (#11)
    * Improve header styling and hide empty prose
    
    * Remove background filter on lg

--- components/Prose.tsx
+++ components/Prose.tsx
@@ -9,7 +9,7 @@ export function Prose({ className, children }: ProseProps) {
   return (
     <div
       className={clsx(
-        'prose prose-lg prose-gray max-w-none dark:prose-invert',
+        'prose prose-lg prose-gray max-w-none dark:prose-invert empty:hidden',
         'prose-headings:relative prose-headings:scroll-mt-28 lg:prose-headings:scroll-mt-12',
         'prose-a:font-semibold prose-a:text-primary-800 prose-a:underline-offset-2 prose-a:transition-colors hover:prose-a:text-primary-600 dark:prose-a:text-primary-500 dark:hover:prose-a:text-primary-700',
         'prose-p:text-gray-700 dark:prose-p:text-gray-300',
