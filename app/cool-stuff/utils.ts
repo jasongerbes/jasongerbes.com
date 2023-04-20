b6279fd4d194a0a69699d0d31c5566730b3ecb4d
Jason Gerbes <j.gerbes@me.com>
2023-04-20 23:56:21 +1200

    Add og:image to cool stuff details page (#12)
    * Add og:image to cool stuff details page
    
    * Adjust margin

---
+++ app/cool-stuff/utils.ts
@@ -0,0 +1,12 @@
+import { CoolThing, allCoolThings } from '@/.contentlayer/generated'
+import { notFound } from 'next/navigation'
+
+export function getCoolThing(id: string): CoolThing {
+  const thing = allCoolThings.find((p) => p.id === id)
+
+  if (!thing || (process.env.NODE_ENV === 'production' && thing.isArchived)) {
+    notFound()
+  }
+
+  return thing
+}
