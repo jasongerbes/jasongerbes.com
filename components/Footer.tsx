0c390ecb35a379e9dc028eceb6ee2087fb28ae8b
Jason Gerbes <j.gerbes@me.com>
2023-04-23 17:54:07 +1200

    Address Lighthouse accessibility issues

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -27,7 +27,7 @@ export function Footer() {
   const year = new Date().getFullYear()
 
   return (
-    <footer className="border-t border-primary-600/10 bg-primary-50/70 px-8 pb-16 pt-16 dark:border-primary-900/30 dark:bg-primary-900/20 sm:px-8 lg:border-none lg:bg-transparent lg:pt-0">
+    <footer className="border-t border-primary-600/10 bg-primary-50/50 px-8 pb-16 pt-16 dark:border-primary-900/30 dark:bg-primary-900/20 sm:px-8 lg:border-none lg:bg-transparent lg:pt-0">
       <div className="mx-auto flex max-w-7xl flex-col justify-between gap-16 sm:items-center lg:justify-center">
         <nav className="lg:hidden">
           <ul className="flex flex-col gap-6 text-base font-semibold sm:flex-row sm:gap-8">
@@ -36,7 +36,7 @@ export function Footer() {
             ))}
           </ul>
         </nav>
-        <p className="text-center text-sm text-primary-700/70 lg:text-gray-500 dark:lg:text-gray-400">
+        <p className="text-center text-sm text-primary-800 dark:text-primary-500 lg:text-gray-500 dark:lg:text-gray-400">
           &copy; {year} Jason Gerbes. All rights reserved.
         </p>
       </div>
@@ -53,7 +53,7 @@ function FooterNavLink({ href, label }: FooterNavLinkProps) {
   return (
     <li>
       <Link
-        className="text-primary-800 transition-colors hover:text-primary-600 dark:text-primary-600 dark:hover:text-primary-700"
+        className="text-primary-800 transition-colors hover:text-primary-600 dark:text-primary-500 dark:hover:text-primary-700"
         href={href}
       >
         {label}
--- components/Tag.tsx
+++ components/Tag.tsx
@@ -11,7 +11,7 @@ export function Tag({ className, icon: Icon, children }: TagProps) {
   return (
     <div
       className={clsx(
-        'inline-flex items-center gap-1 rounded-md bg-gray-300/20 px-2 py-1.5 text-sm font-medium text-gray-500 dark:bg-gray-700/30 dark:text-gray-400',
+        'inline-flex items-center gap-1 rounded-md bg-gray-200/30 px-2 py-1.5 text-sm font-medium text-gray-600 dark:bg-gray-700/30 dark:text-gray-400',
         className
       )}
     >
