e1d060f7982f4eaaddac821026efc9298b97765e
Jason Gerbes <j.gerbes@me.com>
2023-04-26 10:03:18 +1200

    Add Fuse.js to cool stuff list

--- components/input/Checkbox.tsx
+++ components/input/Checkbox.tsx
@@ -11,8 +11,9 @@ export function Checkbox({ label, value, checked, onChange }: CheckboxProps) {
   const id = useId()
 
   return (
-    <div className="inline-flex items-center gap-2">
+    <div className="inline-flex items-center gap-2.5">
       <input
+        className="rounded border-0 bg-transparent text-primary-600 shadow-sm ring-1 ring-inset ring-gray-500/20 focus:bg-gray-200 focus:ring-1 focus:ring-gray-500/40 focus:ring-offset-2 dark:focus:bg-gray-800 dark:focus:ring-offset-gray-900"
         id={id}
         type="checkbox"
         value={value}
@@ -23,3 +24,5 @@ export function Checkbox({ label, value, checked, onChange }: CheckboxProps) {
     </div>
   )
 }
+
+// first-line:border-0 placeholder:text-gray-400 focus:bg-white focus:ring-2 focus:ring-primary-600 dark:text-white dark:ring-gray-500/20 dark:focus:bg-gray-800
--- components/input/Input.tsx
+++ components/input/Input.tsx
@@ -13,7 +13,7 @@ export function Input({ label, value, onChange }: InputProps) {
     <div className="flex flex-col gap-2">
       <label htmlFor={id}>{label}</label>
       <input
-        className="mt-1 block w-full rounded-md border-0 bg-transparent py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-500/10 first-line:border-0 placeholder:text-gray-400 focus:bg-white focus:ring-2 focus:ring-primary-600 dark:text-white dark:ring-gray-500/20 dark:focus:bg-gray-800"
+        className="mt-1 block w-full rounded-md border-0 bg-transparent py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-500/20 placeholder:text-gray-400 focus:bg-gray-50 focus:bg-white focus:ring-primary-600 focus:ring-offset-2 dark:text-white dark:ring-gray-500/20 dark:focus:bg-gray-800 dark:focus:ring-primary-500 dark:focus:ring-offset-gray-900"
         id={id}
         type="text"
         value={value}
