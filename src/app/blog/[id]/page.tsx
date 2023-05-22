deaa00121bb0255e80f4e7e66af09c89195ddd07
Jason Gerbes <j.gerbes@me.com>
2023-05-22 21:48:10 +1200

    Add blog post comments via giscus (#28)

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -5,8 +5,9 @@ import { Markdown } from '@/components/Markdown'
 import { getBlogPost } from '../utils'
 import { BlogPostBadges } from '@/components/blog-posts/BlogPostBadges'
 import { formatDate } from '@/utils/format-date'
-import { PencilSimple } from '@/assets/phosphor-icons'
+import { ClockCounterClockwise } from '@/assets/phosphor-icons'
 import { isSameDay } from 'date-fns'
+import { BlogPostComments } from '@/components/blog-posts/BlogPostComments'
 
 interface Params {
   id: string
@@ -58,10 +59,12 @@ export default function BlogPost({ params }: { params: Params }) {
         <Markdown className="mt-8" code={post.body.code} />
 
         <footer className="mt-16 empty:hidden">
+          <BlogPostComments post={post} />
+
           {hasBeenUpdated && (
-            <p className="inline-flex items-center gap-1.5 text-base font-medium text-gray-500 dark:text-gray-400">
-              <PencilSimple aria-hidden={true} weight="bold" />
-              <span>Updated on {formatDate(post.lastUpdatedDate)}</span>
+            <p className="mt-8 inline-flex w-full items-center gap-1.5 text-base text-gray-500 dark:text-gray-400">
+              <ClockCounterClockwise aria-hidden={true} weight="regular" />
+              <span>Last updated on {formatDate(post.lastUpdatedDate)}</span>
             </p>
           )}
         </footer>
---
+++ src/components/blog-posts/BlogPostComments.tsx
@@ -0,0 +1,34 @@
+'use client'
+import Giscus from '@giscus/react'
+import { BlogPost } from 'contentlayer/generated'
+import { useMedia } from 'react-use'
+
+export interface BlogPostCommentsProps {
+  className?: string
+  post: BlogPost
+}
+
+export function BlogPostComments({ className, post }: BlogPostCommentsProps) {
+  const isDarkTheme = useMedia('(prefers-color-scheme: dark)', false)
+  const theme = isDarkTheme ? 'transparent_dark' : 'light'
+
+  return (
+    <div className={className}>
+      <Giscus
+        id="comments"
+        repo="jasongerbes/jasongerbes.com"
+        repoId="R_kgDOJTxMLQ"
+        category="Comments"
+        categoryId="DIC_kwDOJTxMLc4CWp1e"
+        mapping="specific"
+        term={post.url}
+        reactionsEnabled="1"
+        emitMetadata="0"
+        inputPosition="bottom"
+        theme={theme}
+        lang="en"
+        loading="lazy"
+      />
+    </div>
+  )
+}
